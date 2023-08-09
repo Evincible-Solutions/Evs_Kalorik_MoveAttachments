2eeced0da59aab907dd48e61bae89b6db475f7e7
Bisma Ashraf <Bisma@evinciblesolutions.com>
2023-08-09 17:17:04 +0500

    Add project files.

---
+++ Evs_Kalorik_MoveAttachments/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evs_Kalorik_MoveAttachments
+{
+    public class Class1
+    {
+    }
+}
